9dd3fcd8b0f368adad011de6ce0b42b0bc60c47e
Ben Krig <benjaminjkrig@gmail.com>
2020-04-13 08:35:07 -0700

    assignment7

---
+++ CMPE140-Lab5.srcs/sources_1/new/mux4.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+module mux4 #(parameter WIDTH = 8) (
+        input  wire [1:0]     sel,
+        input  wire [WIDTH-1:0] a,
+        input  wire [WIDTH-1:0] b,
+        input  wire [WIDTH-1:0] c,
+        input  wire [WIDTH-1:0] d, 
+        output reg [WIDTH-1:0]  y
+    );
+    
+    always @ (*)
+    begin
+        case (sel)
+            2'b00: y <= a;
+            2'b01: y <= b;
+            2'b10: y <= c;
+            2'b11: y <= d;
+        endcase
+    end
+
+endmodule
